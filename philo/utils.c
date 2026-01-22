94b75e6f58d8a4a0fab73740e1223f37c838a4ff
este36 <mercierstban@gmail.com>
2026-01-22 16:59:42 +0100

    refactor ft utilities to utils.c

--- philo/utils.c
+++ philo/utils.c
@@ -1,16 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emercier <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*   By: emercier <emercier@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/30 19:54:09 by emercier          #+#    #+#             */
-/*   Updated: 2026/01/21 21:02:25 by emercier         ###   ########.fr       */
+/*   Created: 2026/01/22 16:41:38 by emercier          #+#    #+#             */
+/*   Updated: 2026/01/22 16:42:50 by emercier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	new_len;
+	void	*new;
+
+	new_len = size * nmemb;
+	new = (void *)malloc(new_len);
+	if (!new)
+		return (NULL);
+	ft_bzero((void *)new, new_len);
+	return ((void *)new);
+}
+
+void	*ft_realloc(void *ptr, size_t prev_size, size_t size)
+{
+	void	*new;
+
+	new = malloc(size);
+	if (!new)
+		return (NULL);
+	ft_memcpy(new, ptr, prev_size);
+	free(ptr);
+	return (new);
+}
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
@@ -39,4 +64,25 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 		n--;
 	}
 	return (dest);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*p;
+
+	p = s;
+	while (n && (uintptr_t)p & 7)
+	{
+		*p++ = 0;
+		n--;
+	}
+	while (n >= 8)
+	{
+		*((uint64_t *)p) = 0;
+		p += 8;
+		n -= 8;
+	}
+	while (n--)
+		*p++ = 0;
+	return ;
 }
